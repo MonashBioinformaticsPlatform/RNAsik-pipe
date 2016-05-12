1fc998d61a6b0fd4c5be45df042e9b80f68d97f5
serine <tskiril@gmail.com>
2016-05-12 18:28:28 +1000

    update getGeneIds.py script to also extract gene id, name and biotype from gff. Included that feature into RNAsik. Now gtfFile extention checked and if .gff then switch is made

--- scripts/getGeneIds.py
+++ scripts/getGeneIds.py
@@ -11,12 +11,15 @@ parser = argparse.ArgumentParser(usage='%(prog)s --gtfFile <path/to/your/db_file
                                  add_help=True
                                  )
 parser.add_argument('--gtfFile',
-                    required = True,
                     help=""
+                    )
+parser.add_argument('--gffFile',
+                    help="check"
                     )
 
 args = parser.parse_args()
 gtfFile = args.gtfFile
+gffFile = args.gffFile
 
 genesAttributes = {}
 
@@ -27,40 +30,84 @@ biotypes = {
     "tt": 'transcript_type'
     }
 
-typesRegex = "\s.([A-z0-9_.-]+)"
-
-for key, value in biotypes.items():
-    tweak = value + typesRegex
-    biotypes[key]=tweak
+if gtfFile:
+    typesRegex = "\s.([A-z0-9_.-]+)"
     
-with open(gtfFile) as features:
-    for i in features:
-        if not i.startswith("#"):
-            ninthField = i.split('\t')[8]
+    for key, value in biotypes.items():
+        tweak = value + typesRegex
+        biotypes[key]=tweak
+        
+    with open(gtfFile) as features:
+        for i in features:
+            if not i.startswith("#"):
+                ninthField = i.split('\t')[8]
+    
+                geneId = re.search('gene_id\s.([A-z0-9]+)', ninthField)
+                checkName = re.search('gene_name\s.([A-z0-9_.:-]+)', ninthField)
+    
+                geneName = 'NA'
+                geneBiotype = 'NA'
+    
+                if checkName:
+                    geneName = checkName.group(1)
+    
+                for value in biotypes.values():
+                    checkBiotype = re.search(value, ninthField)
+                    if checkBiotype:
+                        geneBiotype = checkBiotype.group(1)
+    
+                if geneId:
+                    if geneId.group(1) not in genesAttributes:
+                        genesAttributes[geneId.group(1)] = []
+                        genesAttributes[geneId.group(1)].append(geneName)
+                        genesAttributes[geneId.group(1)].append(geneBiotype)
+           
+    header = True
+    for key, value in genesAttributes.items():
+        if header:
+            print '\t'.join(("Gene.ID", "Gene.Name", "Biotype"))
+            header = False
+        print '\t'.join((key, value[0], value[1]))
 
-            geneId = re.search('gene_id\s.([A-z0-9]+)', ninthField)
-            checkName = re.search('gene_name\s.([A-z0-9_.:-]+)', ninthField)
-
-            geneName = 'NA'
-            geneBiotype = 'NA'
-
-            if checkName:
-                geneName = checkName.group(1)
-
-            for value in biotypes.values():
-                checkBiotype = re.search(value, ninthField)
-                if checkBiotype:
-                    geneBiotype = checkBiotype.group(1)
-
-            if geneId:
-                if geneId.group(1) not in genesAttributes:
-                    genesAttributes[geneId.group(1)] = []
-                    genesAttributes[geneId.group(1)].append(geneName)
-                    genesAttributes[geneId.group(1)].append(geneBiotype)
-       
-header = True
-for key, value in genesAttributes.items():
-    if header:
-        print '\t'.join(("Gene.ID", "Gene.Name", "Biotype"))
-        header = False
-    print '\t'.join((key, value[0], value[1]))
+# Get gene Ids from GFF file format
+if gffFile:
+    typesRegex = "=([A-z0-9_.-]+)"
+    
+    for key, value in biotypes.items():
+        tweak = value + typesRegex
+        biotypes[key]=tweak
+    
+    with open(gffFile) as features:
+        for i in features:
+            line = i.strip()
+            if not line.startswith('#'):
+                feature = line.split('\t')
+                if feature[2] == 'gene':
+                    ninthField = feature[8]
+    
+                    geneId = re.search('ID=([A-z0-9]+)', ninthField)
+                    checkName = re.search('Name=([A-z0-9_.:-]+)', ninthField)
+    
+                    geneName = 'NA'
+                    geneBiotype = 'NA'
+    
+                    if checkName:
+                        geneName = checkName.group(1)
+    
+                    for value in biotypes.values():
+                        checkBiotype = re.search(value, ninthField)
+                        if checkBiotype:
+                            geneBiotype = checkBiotype.group(1)
+    
+                    if geneId:
+                        if geneId.group(1) not in genesAttributes:
+                            genesAttributes[geneId.group(1)] = []
+                            genesAttributes[geneId.group(1)].append(geneName)
+                            genesAttributes[geneId.group(1)].append(geneBiotype)
+           
+    header = True
+    for key, value in genesAttributes.items():
+        if header:
+            print '\t'.join(("Gene.ID", "Gene.Name", "Biotype"))
+            header = False
+        print '\t'.join((key, value[0], value[1]))
